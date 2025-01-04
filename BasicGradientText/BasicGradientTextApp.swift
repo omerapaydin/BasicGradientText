43f7f9c22e70d2252dda70c5a0b7d9d2a65547a6
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-04 17:46:09 +0300

    Initial Commit

---
+++ BasicGradientText/BasicGradientTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicGradientTextApp.swift
+//  BasicGradientText
+//
+//  Created by Ömer on 4.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BasicGradientTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicGradientTextTests/BasicGradientTextTests.swift
@@ -0,0 +1,17 @@
+//
+//  BasicGradientTextTests.swift
+//  BasicGradientTextTests
+//
+//  Created by Ömer on 4.01.2025.
+//
+
+import Testing
+@testable import BasicGradientText
+
+struct BasicGradientTextTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
